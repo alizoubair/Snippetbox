7cd8bf4caaba878ab0c8bc510363f78461557d94
Ali Zoubair <88006585+alizoubair@users.noreply.github.com>
2023-09-28 15:39:36 +0100

    Unit test the humanDate()

--- cmd/web/templates_test.go
+++ cmd/web/templates_test.go
@@ -15,17 +15,17 @@ func TestHumandDate(t *testing.T) {
 	}{
 		{
 			name: "UTC",
-			tm: time.Date(2023, 3, 17, 10, 15, 0, 0, time.UTC),
+			tm:   time.Date(2023, 3, 17, 10, 15, 0, 0, time.UTC),
 			want: "17 Mar 2023 at 10:15",
 		},
 		{
 			name: "Empty",
-			tm: time.Time{},
+			tm:   time.Time{},
 			want: "",
 		},
 		{
 			name: "CET",
-			tm: time.Date(2023, 3, 28, 14, 40, 0, 0, time.FixedZone("CET", 1*60*60)),
+			tm:   time.Date(2023, 3, 28, 14, 40, 0, 0, time.FixedZone("CET", 1*60*60)),
 			want: "28 Mar 2023 at 13:40",
 		},
 	}
