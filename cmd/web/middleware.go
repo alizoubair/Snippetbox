4cfc0d6ed23b7cf58ebaaa1028d247d1325cb5c3
Ali Zoubair <88006585+alizoubair@users.noreply.github.com>
2023-09-28 12:44:42 +0100

    Add support for the request context

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 
@@ -70,4 +71,29 @@ func noSurf(next http.Handler) http.Handler {
 	})
 
 	return csrfHandler
+}
+
+// authenticate middleware method uses the request context for authentication
+// to check the database for the ID using the UserModel.Exists method.
+func (app *application) authenticate(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		id := app.sessionManager.GetInt(r.Context(), "authenticationID")
+		if id == 0 {
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		exists, err := app.users.Exists(id)
+		if err != nil {
+			app.serverError(w, err)
+			return
+		}
+
+		if exists {
+			ctx := context.WithValue(r.Context(), isAuthenticatedContextKey, true)
+			r = r.WithContext(ctx)
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
--- internal/models/users.go
+++ internal/models/users.go
@@ -68,3 +68,13 @@ func (m *UserModel) Authenticate(email, password string) (int, error) {
 
 	return id, nil
 }
+
+
+func (m *UserModel) Exists(id int) (bool, error) {
+	var exists bool
+
+	query := `SELECT EXISTS(SELECT true FROM users WHERE id = ?)`
+
+	err := m.DB.QueryRow(query, id).Scan(&exists)
+	return exists, err
+}
